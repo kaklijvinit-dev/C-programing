c901b9631c1105b0a8ea34d6a87606cf2cd90abb
Vinit M. Kaklij <kaklijvinit@gmail.com>
2025-12-30 21:40:30 +0530

    Update and rename pratical5.c to pratical5(1).c

---
+++ pratical5(1).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+    // using nested condition operator
+    (a > 0) ? printf("Positive\n"):
+    (a < 0) ? printf("Negative\n"):
+    printf("Zero\n");
+    return 0;
+}
+
