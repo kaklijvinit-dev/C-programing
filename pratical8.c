31fe4bf43c60f571f567e4b2ae9eb0f495ced3bf
Vinit M. Kaklij <kaklijvinit@gmail.com>
2026-01-09 09:26:03 +0530

    Add files via upload

---
+++ pratical8.c
@@ -0,0 +1,13 @@
+//Implement minimum two C programs using Bitwise Operators
+#include <stdio.h>
+int main()
+{
+    int a=6,b=4;
+    printf("Bitwise And(a&b)=%d\n",a&b);
+    printf("Bitwise Or(a|b)=%d\n",a|b);
+    printf("Bitwise Xor(a^b)=%d\n",a^b);
+    printf("Bitwise Not(~a)=%d\n",~a);
+    printf("Left Shift(a<<1)=%d\n",a<<1);
+    printf("Right Shift(a>>1)=%d\n",a>>1);
+    return 0;
+}
